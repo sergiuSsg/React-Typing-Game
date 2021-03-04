83b4757f8363c8faa1d357026c4869f92379fa20
Sergiu Pop <52707966+sergiuSsg@users.noreply.github.com>
2021-03-04 20:35:00 +0000

    Update App.js

--- App.js
+++ App.js
@@ -1,31 +1,86 @@
-import React, { useState, useEffect, useRef } from "react"
-import useWordGame from "./hooks/useWordGame"
+import React from "react"
+import { useState, useEffect, useRef } from "react"
+import BottomBar from "./components/BottomBar";
+import TextArea from "./components/TextArea"
+import TopBar from "./components/TopBar"
+import HighScores from "./components/HighScores";
 import "./styles.css"
 
+
 function App() {
-    const {
-        textAreaRef,
-        isRunning,
-        changeHandler,
-        text,
-        timeRemaining,
-        startGame,
-        wordCount,
-        START_TIME
-    } = useWordGame(5)
+
+    const localData = localStorage.getItem('arrayTest') 
+
+    const parsedLocalData = localData ? JSON.parse(localData) : []
+
+
+    const [scoreArray, setScoreArray] = useState(parsedLocalData) 
+
+
+    const START_TIME = 5;
+    const [text, setText] = useState("");
+    const [timeRemaining, setTimeRemaining] = useState(null);
+    const [isRunning, setIsRunning] = useState(false);    
+    const [updateScore, setUpdateScore] = useState(false);
+    const [wordCount, setWordCount] = useState(null);
+    const textAreaRef = useRef(null);
+
+    if(timeRemaining === 0 && updateScore ){
+        setScoreArray((oldScoreArray) => [...oldScoreArray, wordCount])
+        setUpdateScore(false)
+    }
+
+    useEffect(() => {
+        localStorage.setItem('arrayTest', JSON.stringify(scoreArray))
+    }, [scoreArray])
+
+    function changeHandler(event) {
+        const { value } = event.target
+        setText(value)
+    }
+
+    function wordCounter(string) {
+        const counter = string.trim().split(" ").filter(word => word !== "")
+        return counter.length
+    }
+
+    function startGame() {
+        setIsRunning(true)
+        setTimeRemaining(START_TIME)
+        setWordCount(0)
+        setText("")
+        textAreaRef.current.disabled = false;
+        textAreaRef.current.focus()
+    }
+
+    useEffect(() => {
+        if (isRunning && timeRemaining > 0) {
+            const time = setTimeout(() => {
+                setTimeRemaining(prevTime => prevTime - 1)
+            }, 1000)
+            return () => {
+                clearTimeout(time);
+            }
+        }  else if (timeRemaining === 0) {
+            setIsRunning(false)
+            setWordCount(wordCounter(text))
+            setUpdateScore(true)
+        }
+    }, [timeRemaining])
 
     return (
         <div>
-            <h1>Speed Typing Game</h1>
-            <textarea ref={textAreaRef} disabled={!isRunning} onChange={changeHandler} value={text} />
-            <h4>Time Remaining: {timeRemaining ? timeRemaining : "Time's up!"}</h4>
-            <button disabled={isRunning} onClick={startGame}>Start Game</button>
-            <h1>
-                {
-                    !wordCount ? `Word Count: ${wordCount}` :
-                        `Word Count: ${wordCount} words in ${START_TIME} seconds!`
-                }
-            </h1>
+            <TopBar />
+            <TextArea textAreaRef={textAreaRef} isRunning={isRunning} changeHandler={changeHandler} text={text}/>
+            <BottomBar 
+                startGame={startGame} 
+                timeRemaining={timeRemaining} 
+                isRunning={isRunning} 
+                wordCount={wordCount} 
+                startTime={START_TIME}
+                setScoreArray={setScoreArray}
+            />
+            <HighScores scoreArray={scoreArray}/>
         </div>
     )
 }
