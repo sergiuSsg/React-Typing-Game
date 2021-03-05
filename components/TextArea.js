f5bb70d98131c5fb48094fc1135b98ef513cdb1b
Sergiu Pop <52707966+sergiuSsg@users.noreply.github.com>
2021-03-05 21:03:59 +0000

    Delete TextArea.js

--- components/TextArea.js
+++
@@ -1,8 +0,0 @@
-import React from "react"
-
-
-function TextArea({textAreaRef, isRunning, changeHandler, text}) {
-    return <textarea ref={textAreaRef} disabled={!isRunning} onChange={changeHandler} value={text} />
-}
-
-export default TextArea
