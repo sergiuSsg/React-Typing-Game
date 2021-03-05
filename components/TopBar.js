0a2de59584444bb226f79952facbbb2b04f35cbc
Sergiu Pop <52707966+sergiuSsg@users.noreply.github.com>
2021-03-05 21:07:01 +0000

    Update TopBar.js

--- components/TopBar.js
+++ components/TopBar.js
@@ -1,8 +1,12 @@
 import React from "react"
 
-function TopBar() {
-    return <h1>Speed Typing Game</h1>
-
+function TopBar({startTime}) {
+    return (
+    <div>
+        <h1>Speed Typing Game</h1>
+        <h4>How many words can you type in {startTime} seconds?</h4>
+    </div>
+    )
 }
 
 export default TopBar
