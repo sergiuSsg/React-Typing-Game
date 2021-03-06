972ce95c940ae1f9df74bb961905584835bae1b1
Sergiu Pop <52707966+sergiuSsg@users.noreply.github.com>
2021-03-06 07:17:41 +0000

    Update HighScores.js

--- components/HighScores.js
+++ components/HighScores.js
@@ -2,7 +2,7 @@ import React from "react"
 import shortid from 'shortid';
 
 function HighScores({scoreArray}) {
-
+    
     const mappedScore = scoreArray.sort((a, b) => b - a).map((score) => {
         return <li key={shortid.generate()} >{score} {scoreArray.length > 0 ? "Words!" : "--------"}</li>
     }).slice(0, 5)
